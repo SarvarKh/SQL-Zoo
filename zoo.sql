aa62c4661b9be3aba83fcce3dadfa0098d1bfdf8
SarvarKhalimov <60043698+SarvarKh@users.noreply.github.com>
2021-01-09 00:59:16 +0500

    Complete remaining tasks from Tutorial-2

--- zoo.sql
+++ zoo.sql
@@ -102,6 +102,30 @@ SELECT name
  WHERE name LIKE capital
 
 
+-- 12-Task solution:
+SELECT name
+  FROM world
+ WHERE capital = concat(name , ' City')
+
+
+-- 13-Task solution:
+SELECT capital, name
+FROM world
+WHERE capital LIKE concat(name, '%')
+
+
+-- 14-Task solution:
+SELECT capital, name
+FROM world
+WHERE capital LIKE concat(name, '%') AND NOT capital = name
+
+
+-- 15-Task solution:
+SELECT name, REPLACE(capital, name, '') AS ext
+FROM world
+WHERE capital LIKE concat(name, '_%')
+
+
 
 /*
 ***************************************************************
