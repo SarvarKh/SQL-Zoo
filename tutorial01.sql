a10fdd1bf7d756625a03a3b00d71e1cda966ad83
SarvarKhalimov <60043698+SarvarKh@users.noreply.github.com>
2021-01-09 22:10:55 +0500

    Move tutorials' tasks to dedicated files

---
+++ tutorial01.sql
@@ -0,0 +1,26 @@
+-- SQL tasks from sqlzoo.net
+
+/*
+***************************************************************
+**                                                           **
+***** *****            TUTORIAL - 1            ***** *****   **
+**                                                           **
+***************************************************************
+*/
+
+-- 1-Task solution:
+SELECT population 
+FROM world
+  WHERE name = 'Germany'
+
+
+-- 2-Task solution:
+SELECT name, population 
+FROM world
+  WHERE name IN ('Sweden', 'Norway', 'Denmark');
+
+
+-- 3-Task solution:
+SELECT name, area 
+FROM world
+  WHERE area BETWEEN 200000 AND 250000
--- tutorial02.sql
+++ tutorial02.sql
@@ -3,32 +3,6 @@
 /*
 ***************************************************************
 **                                                           **
-***** *****            TUTORIAL - 1            ***** *****   **
-**                                                           **
-***************************************************************
-*/
-
--- 1-Task solution:
-SELECT population 
-FROM world
-  WHERE name = 'Germany'
-
-
--- 2-Task solution:
-SELECT name, population 
-FROM world
-  WHERE name IN ('Sweden', 'Norway', 'Denmark');
-
-
--- 3-Task solution:
-SELECT name, area 
-FROM world
-  WHERE area BETWEEN 200000 AND 250000
-
-
-/*
-***************************************************************
-**                                                           **
 ***** *****            TUTORIAL - 2            ***** *****   **
 **                                                           **
 ***************************************************************
