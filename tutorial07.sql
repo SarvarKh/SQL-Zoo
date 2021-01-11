f572b03a47b606b8f1b3c0b8146c45fccdbe1074
SarvarKhalimov <60043698+SarvarKh@users.noreply.github.com>
2021-01-11 21:46:55 +0500

    Solve till task 11, tutorial 7

--- tutorial07.sql
+++ tutorial07.sql
@@ -46,6 +46,33 @@ WHERE movieid=11768
 
 
 -- 7-Task solution:
+SELECT name
+FROM casting JOIN actor ON casting.actorid = actor.id
+WHERE movieid = (SELECT id FROM movie WHERE title = 'Alien')
 
 
 -- 8-Task solution:
+SELECT title
+FROM casting 
+  JOIN movie ON casting.movieid = movie.id
+  JOIN actor ON casting.actorid = actor.id
+WHERE name = 'Harrison Ford'
+
+
+-- 9-Task solution:
+SELECT title
+FROM casting 
+  JOIN movie ON casting.movieid = movie.id
+  JOIN actor ON casting.actorid = actor.id
+WHERE name = 'Harrison Ford' AND ord != 1
+
+
+-- 10-Task solution:
+SELECT title, name
+FROM casting 
+  JOIN movie ON casting.movieid = movie.id
+  JOIN actor ON casting.actorid = actor.id
+WHERE yr = 1962 AND ord = 1
+
+
+-- 11-Task solution:
