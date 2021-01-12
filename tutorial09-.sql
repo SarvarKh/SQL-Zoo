4a52fcd57b2b2044addfc6fea64141b9a271a262
SarvarKhalimov <60043698+SarvarKh@users.noreply.github.com>
2021-01-12 18:37:23 +0500

    Finish tuturial 9-, Window function

--- tutorial09-.sql
+++ tutorial09-.sql
@@ -9,3 +9,52 @@
 */
 
 -- 1-Task solution:
+SELECT lastName, party, votes
+  FROM ge
+ WHERE constituency = 'S14000024' AND yr = 2017
+ORDER BY votes DESC
+
+
+-- 2-Task solution:
+SELECT party, votes,
+       RANK() OVER (ORDER BY votes DESC) as posn
+  FROM ge
+ WHERE constituency = 'S14000024' AND yr = 2017
+ORDER BY party
+
+
+-- 3-Task solution:
+SELECT yr,party, votes,
+      RANK() OVER (PARTITION BY yr ORDER BY votes DESC) as posn
+  FROM ge
+ WHERE constituency = 'S14000021'
+ORDER BY party,yr
+
+
+-- 4-Task solution:
+SELECT constituency,party, votes,
+  RANK() OVER (PARTITION BY constituency ORDER BY votes DESC) as posn
+  FROM ge
+ WHERE constituency BETWEEN 'S14000021' AND 'S14000026'
+   AND yr  = 2017
+ORDER BY posn, constituency
+
+
+-- 5-Task solution:
+select  constituency,party from 
+(
+SELECT constituency,party, votes , 
+rank() OVER (PARTITION BY constituency order by votes desc) rn
+  FROM ge
+ WHERE constituency BETWEEN 'S14000021' AND 'S14000026'
+   AND yr  = 2017
+ORDER BY constituency,votes DESC
+) TAB WHERE rn =1
+
+
+-- 6-Task solution:
+SELECT party , COUNT(*)
+FROM ge x 
+WHERE constituency like 'S%'
+AND yr  = 2017 AND votes >= ALL(SELECT votes FROM ge y  WHERE x.constituency = y. constituency AND y.yr = 2017)
+GROUP BY party
