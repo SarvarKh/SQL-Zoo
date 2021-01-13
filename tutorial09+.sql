177806ca97639f825312bf387cc09795a98e9f98
SarvarKhalimov <60043698+SarvarKh@users.noreply.github.com>
2021-01-13 14:45:46 +0500

    Finish tutorial 9+, covid

--- tutorial09+.sql
+++ tutorial09+.sql
@@ -68,7 +68,13 @@ ORDER BY confirmed DESC
 
 
 -- 7-Task solution:
-
+SELECT 
+   world.name,
+   ROUND(100000*confirmed/population,0),
+   RANK() OVER (ORDER BY confirmed/population) 'rank'
+  FROM covid JOIN world ON covid.name=world.name
+WHERE whn = '2020-04-20' AND population > 10000000
+ORDER BY population DESC
 
 
 -- 8-Task solution:
